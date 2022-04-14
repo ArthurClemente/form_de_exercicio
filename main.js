b8e0463d023a63bf5d8358e1f314d7133a13c0b4
ArthurClemente <arthurclementte@hotmail.com>
2022-04-14 20:15:13 -0300

    Criadas as funções para esconder e mostrar o campo de idade do formulário.

---
+++ main.js
@@ -0,0 +1,16 @@
+function esconderIdade() {
+    let p_idade = document.getElementById("p_idade");
+    let idade = document.getElementById("idade");
+
+    p_idade.hidden = true;
+    idade.hidden = true;
+}
+
+function mostrarIdade() {
+    let p_idade = document.getElementById("p_idade");
+    let idade = document.getElementById("idade");
+
+    p_idade.hidden = false;
+    idade.hidden = false;
+}
+
